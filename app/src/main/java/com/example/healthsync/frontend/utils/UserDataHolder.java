821f04f71c6b4ae2f2e37553fa3ce606eb3e3946
saimanohargithub <saimanohar@example.com>
2026-06-13 12:04:00 +0530

    Remove all files from repository

--- app/src/main/java/com/example/healthsync/frontend/utils/UserDataHolder.java
+++
@@ -1,7 +0,0 @@
-package com.example.healthsync.frontend.utils;
-
-import com.example.healthsync.frontend.models.UserProfile;
-
-public class UserDataHolder {
-    public static UserProfile userProfile = new UserProfile();
-}
